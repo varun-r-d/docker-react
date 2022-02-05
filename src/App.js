1a23cf88a976a477074f894d0c7bbac3e29afac9
varun-r-d <varun.ramesh.d@gmail.com>
2022-02-06 01:29:36 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload      </p>
+          Edit <code>src/App.js</code> and save to reload , I was changes on feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
