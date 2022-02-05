f94e7aca439d9e82be1a77bb776185e4e8e2d8f0
varun-r-d <varun.ramesh.d@gmail.com>
2022-02-06 01:13:55 +0530

    app file update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Bye there buddy!       </p>
+          Edit <code>src/App.js</code> and save to reload      </p>
         <a
           className="App-link"
           href="https://reactjs.org"
